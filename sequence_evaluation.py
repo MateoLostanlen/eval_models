3c6a5be45f67133b9bf0f7cbf4e9751352a8d42c
theocayla <theocayla@hotmail.fr>
2025-03-26 20:09:07 +0100

    Add functions to separate captures by session

--- sequence_evaluation.py
+++ sequence_evaluation.py
@@ -2,10 +2,12 @@ import glob
 import logging
 import json
 import os
-import time
+import re
 
 import numpy as np
 import pandas as pd
+
+from datetime import datetime, timedelta
 
 from PIL import Image
 from tqdm import tqdm 
@@ -17,7 +19,35 @@ ext = [".jpg", ".png", ".tif", ".jpeg", ".tiff"]
 def is_image(image_path):
     return os.path.isfile(image_path) and os.path.splitext(image_path)[-1] in ext
 
-def run_engine_predictions(image_folder, outpath, dConfig, save_pred=True, resume=True):
+def run_engine_session(config, session_name, image_paths, labels):
+    """
+    Instanciate an Engine and run predictions on a list of images.
+    Returns a dataframe containing image info and the confidence predicted
+    """
+    pyroEngine = Engine(
+        nb_consecutive_frames=config.get("nb_consecutive_frames", 4),
+        conf_thresh=config.get("conf_thresh", 0.15),
+        max_bbox_size=config.get("max_bbox_size", 0.4),
+        model_path=config.get("model_path", None)
+    )
+
+    session_results = pd.DataFrame(columns=["session", "image", "session_label", "prediction", "conf"])
+
+    for file, label in zip(image_paths, labels):
+        im = Image.open(file)
+        conf = pyroEngine.predict(im)
+        session_results.loc[len(session_results)] = [
+            session_name,
+            file,
+            label != "fp",
+            conf > pyroEngine.conf_thresh,
+            conf
+        ]
+
+    return session_results
+
+
+def run_engine_dataset(image_folder, outpath, dConfig, save_pred=True, resume=True):
     """
     Functions that process predictions through the Engine on folders of images
     Args
@@ -29,7 +59,7 @@ def run_engine_predictions(image_folder, outpath, dConfig, save_pred=True, resum
             - nb_consecutive_frames (4 by default)
             - conf_thresh (0.15 by default)
             - max_bbox_size (0.4 by default)
-            - model_path (None by default)
+            - model_path : onnx format only (None by default)
         save_pred : optional param that activates file saving in 
         resume : optional param that allows to load previous predictions instead of starting from scratch
     Saves results in a csv file
@@ -70,32 +100,109 @@ def run_engine_predictions(image_folder, outpath, dConfig, save_pred=True, resum
             for session in tqdm(os.listdir(os.path.join(image_folder, label))):
                 if session in set(data["session"].to_list()):
                     continue
-
-                # Nex instance of pyro engine for each image sequence
-                pyroEngine = Engine(
-                    nb_consecutive_frames=config.get("nb_consecutive_frames", 4),
-                    conf_thresh=config.get("conf_thresh", 0.15),
-                    max_bbox_size=config.get("max_bbox_size", 0.4),
-                    model_path=config.get("model_path", None)
-                )
-
-                for file in glob.glob(f"{os.path.join(image_folder, label, session)}/*"):
-                    im = Image.open(file)
-                    conf = pyroEngine.predict(im)
-                    data.loc[len(data)] = [
-                        session,
-                        os.path.basename(file),
-                        label != "fp",
-                        conf > pyroEngine.conf_thresh,
-                        conf
-                    ]
-
-                    # Checkpoint to save predictions
-                    if save_pred and len(data) % 50 == 0:
-                        data.to_csv(outCsv, index=False)
+                session_folder = os.path.join(image_folder, label, session)
+                image_paths = [
+                    os.path.join(session_folder, file)
+                    for file in os.listdir(session_folder)
+                    if is_image(os.path.join(session_folder, file))
+                ]
+                labels = [label for image in image_paths]
+                session_results = run_engine_session(config, session, image_paths, labels)
+                
+                # Add session results to master result file
+                data = pd.concat(data, session_results)
+                # Checkpoint to save predictions
+                if save_pred and len(data) % 50 == 0:
+                    data.to_csv(outCsv, index=False)
         if save_pred:
             data.to_csv(outCsv, index=False)
+
+def parse_date_from_filename(filename):
+    '''Extracts date from filename, typcally : pyronear_sdis-07_brison-200_2024-01-26t11-13-37.jpg'''
+    pattern = r'_(\d{4})_(\d{2})_(\d{2})t(\d{2})_(\d{2})_(\d{2})\.(jpg|png)$'
     
+    # Search for the pattern in the filename
+    match = re.search(pattern, filename.lower())
+    
+    if match:
+        # Extract components
+        year = int(match.group(1))
+        month = int(match.group(2))
+        day = int(match.group(3))
+        hour = int(match.group(4))
+        minute = int(match.group(5))
+        second = int(match.group(6))
+        
+        # Create datetime object
+        file_datetime = datetime(year, month, day, hour, minute, second)
+        return file_datetime
+    
+    return None
+
+def determine_sessions(image_folder, annotation_folder, dataset_name):
+    '''
+    Parse images to detect files belonging to the same session by comparing camera name and capture dates.
+    Works with wildfire2025 and DS_fp formats (expects file named as *_year_month_daythour_)
+    '''
+    image_files = glob.glob(os.path.join(image_folder, '*.jpg'))
+    image_files.sort()
+
+    data = []
+    current_session = None
+    session_images = []
+
+    for image_file in image_files:
+        image_date = parse_date_from_filename(image_file)
+        if not is_image(image_file) or not image_date:
+            continue
+
+        if not current_session:
+            current_session = os.path.splitext(os.path.basename(image_file))[0]
+            session_images = [image_file]
+        else:
+            last_image_date = parse_date_from_filename(session_images[-1])
+            if (image_date - last_image_date) <= timedelta(minutes=30):
+                session_images.append(image_file)
+            else:
+                # More than 30 min between two captures -> Save current session and start a new one
+                for img in session_images:
+                    annotation_file = os.path.join(annotation_folder, os.path.splitext(os.path.basename(img))[0] + '.txt')
+                    if not os.path.isfile(annotation_file):
+                        annotations = ""
+                    else:
+                        with open(annotation_file, 'r') as file:
+                            annotations = file.read()
+                    data.append({
+                        'image': img,
+                        'session': current_session,
+                        'label': annotations,
+                        'original_dataset': dataset_name
+                    })
+                current_session = os.path.splitext(os.path.basename(image_file))[0]
+                session_images = [image_file]
+
+    # Save last session
+    if session_images:
+        for img in session_images:
+            annotation_file = os.path.join(annotation_folder, os.path.splitext(os.path.basename(img))[0] + '.txt')
+            if not os.path.isfile(annotation_file):
+                    annotations = ""
+            else:
+                with open(annotation_file, 'r') as file:
+                    annotations = file.read()
+            data.append({
+                'image': img,
+                'session': current_session,
+                'label': annotations,
+                'original_dataset': dataset_name
+            })
+
+    # Store everything in a csv
+    df = pd.DataFrame(data)
+    output_csv = os.path.join(os.path.dirname(image_folder), f"{os.path.basename(image_folder)}.csv")
+    df.to_csv(output_csv, index=False)
+    print(f"DataFrame saved in {output_csv}")
+
 def concat_preds(datapath):
     '''
     Concatenate predictions from existing result files.
@@ -220,7 +327,7 @@ if __name__ == "__main__":
         },
     }
 
-    run_engine_predictions(image_folder, datapath, config, save_pred=True)
+    run_engine_dataset(image_folder, datapath, config, save_pred=True)
     concat_preds(datapath)
     compute_accuracy(datapath)
     
